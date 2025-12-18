5b2217033e029248a1fe4e9efe3453c99e0d4f1f
John Aquino <41652412+apollyon600@users.noreply.github.com>
2025-12-18 06:46:06 -0700

    feat: implement CommandHelper for 11.1.0 update

---
+++ src/main/java/gg/lode/bookshelfcmd/util/CommandHelper.java
@@ -0,0 +1,25 @@
+package gg.lode.bookshelfcmd.util;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CommandHelper {
+
+    public static OfflinePlayer getOfflinePlayerOrPlayer(UUID uniqueId) {
+        return uniqueId == null ? null : Bukkit.getPlayer(uniqueId) != null ? Bukkit.getPlayer(uniqueId) : Bukkit.getOfflinePlayer(uniqueId);
+    }
+
+    @Nullable
+    public static OfflinePlayer convertPlayerProfileToOfflinePlayer(List<PlayerProfile> profiles) {
+        PlayerProfile playerProfile = profiles.size() > 0 ? profiles.get(0) : null;
+        if (playerProfile == null || playerProfile.getUniqueId() == null) return null;
+
+        return getOfflinePlayerOrPlayer(playerProfile.getUniqueId());
+    }
+
+}
